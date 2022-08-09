05935a134c353b23f541c4fcf3dbf05daf842e00
Alesia <alesia.litvinenko@gmail.com>
2022-08-09 20:00:24 +0300

    fead: bracket

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  if (str.length % 2 !== 0) {
+    return false
+  }
+
+  const reversedBracketsConfig = bracketsConfig.map((brackets) =>
+    [...brackets].reverse()
+  )
+  const bracketsMap = Object.fromEntries(reversedBracketsConfig)
+
+  const prevBracketsArray = []
+
+  for (let i = 0; i < str.length; i++) {
+    const currentBracket = str[i]
+
+    if (!prevBracketsArray.length) {
+      prevBracketsArray.push(currentBracket)
+      continue
+    }
+
+    const [prevBracket] = prevBracketsArray.slice(-1)
+
+    if (prevBracket === bracketsMap[currentBracket]) {
+      prevBracketsArray.pop()
+      continue
+    }
+
+    prevBracketsArray.push(currentBracket)
+  }
+
+  return !prevBracketsArray.length
 }
+
